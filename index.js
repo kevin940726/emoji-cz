540bec068965f9ebceda07db4163dc238c9a4587
kevin940726 <kevin830726@gmail.com>
2017-03-06 00:07:22 +0800

    🐛 Fix: use lodash merge to merge configs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "format cjs";
 
+const merge = require('lodash.merge');
 const engine = require('./engine');
 const types = require('./types.json');
 const getConfig = require('./getConfig');
@@ -8,6 +9,6 @@ const config = getConfig();
 
 module.exports = engine({
   types: config && config.types ?
-    Object.assign({}, types, config.types) :
+    merge(types, config.types) :
     types,
 });
